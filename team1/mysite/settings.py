7b0305b7b349735004cb9dc43127eebfb84a0e20
Ciel <qiuhongyu007@qq.com>
2015-08-18 23:44:36 +0800

    Qiu Hongyu 1st commit-modify the title and combine the code

--- team1/mysite/settings.py
+++ team1/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7&^r*59v)!&k2x)tnuxguonnkvek!q^q$@dot8_vkana8694$i'
+SECRET_KEY = 'j=v#=qf0puuiz&@hnu05+ru+-j**p(i@dd0$j9mb7mow^9=1g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sina' #New
+    'sina'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static").replace('\\', '/'),
+)
