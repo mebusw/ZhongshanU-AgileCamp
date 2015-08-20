3b3fcd4d8f35c7ce99485fe0522f324591c7d8fb
Jamoeba <Jamoeba@live.com>
2015-08-20 12:50:34 +0800

    同步组员修改内容

--- mysite/sina/views.py
+++ mysite/sina/views.py
@@ -15,7 +15,14 @@ def index ( request ):
 	name_list = []
 	for i in place_list:
 		name_list.append(i.name)
+
+	maxplace_name = ''
+	max_counter = 0 
+	for i in name_list:
+		if max_counter < name_list.count(i) :
+			max_counter = name_list.count(i)
+			maxplace_name = i   
+
 	
 	return render(request, 'sina/index.html' ,
-		{ 'pla':pla	, 'list':name_list[1:] } ) 
-
+		{ 'pla':pla	, 'list':name_list[2:] , 'maybe_place' :maxplace_name } ) 
